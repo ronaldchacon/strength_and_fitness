064afe29b1cb98d869ebc67a1f676ad64c6e298b
Ronald Chacon <ronald.chacon87@gmail.com>
2016-12-13 20:52:31 -0800

    Add kaminari

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'active_model_serializers'
+gem 'kaminari'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,6 @@
 class ExercisesController < ApplicationController
   def index
-    @exercises = Exercise.all
+    @exercises = Exercise.page(params[:page]).per(25)
     render json: @exercises,
            include: ['equipment', 'muscles', 'exercise_types'],
            status: :ok
